573f5b883ef705183631f3834e64c872aa36970f
MurataSakuya <144077681+Muratasakuya@users.noreply.github.com>
2025-04-06 00:06:10 +0900

    scene変数の削除

--- Project/Assets/Engine/Shaders/VS_PS/Object/Object3D.PS.hlsl
+++ Project/Assets/Engine/Shaders/VS_PS/Object/Object3D.PS.hlsl
@@ -61,206 +61,203 @@ PSOutput main(VSOutput input) {
 
 	PSOutput output;
 	
-	output.color.rgb = input.normal * 0.5f + 0.5f;
-	output.color.a = 1.0f;
+	float4 transformUV = mul(float4(input.texcoord, 0.0f, 1.0f), uvTransform);
+	float4 textureColor = gTexture.Sample(gSampler, transformUV.xy);
 	
-	//float4 transformUV = mul(float4(input.texcoord, 0.0f, 1.0f), uvTransform);
-	//float4 textureColor = gTexture.Sample(gSampler, transformUV.xy);
+	//========================================================================*/
+	//* Lighting *//
 	
-	////========================================================================*/
-	////* Lighting *//
-	
-	//if (enableLighting == 1) {
+	if (enableLighting == 1) {
 		
-	//	if (enableHalfLambert == 1) {
+		if (enableHalfLambert == 1) {
 
-	//		float NdotL = dot(normalize(input.normal), normalize(-directionalLight.direction));
-	//		float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
+			float NdotL = dot(normalize(input.normal), normalize(-directionalLight.direction));
+			float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
 			
-	//		// rgb
-	//		output.color.rgb = color.rgb * textureColor.rgb * directionalLight.color.rgb * cos * directionalLight.intensity;
-	//	} else {
+			// rgb
+			output.color.rgb = color.rgb * textureColor.rgb * directionalLight.color.rgb * cos * directionalLight.intensity;
+		} else {
 
-	//		float cos = saturate(dot(normalize(input.normal), -directionalLight.direction));
+			float cos = saturate(dot(normalize(input.normal), -directionalLight.direction));
 			
-	//		// rgb
-	//		output.color.rgb =
-	//		color.rgb * textureColor.rgb * directionalLight.color.rgb * cos * directionalLight.intensity;
-	//	}
+			// rgb
+			output.color.rgb =
+			color.rgb * textureColor.rgb * directionalLight.color.rgb * cos * directionalLight.intensity;
+		}
 		
-	//	if (enablePhongReflection == 1) {
+		if (enablePhongReflection == 1) {
 			
-	//		// PointLight‚Ì“üŽËŒõ
-	//		float3 pointLightDirection = normalize(input.worldPosition - pointLight.pos);
-	//		// PointLight‚Ö‚Ì‹——£
-	//		float distancePointLight = length(pointLight.pos - input.worldPosition);
-	//		// Žw”‚É‚æ‚éƒRƒ“ƒgƒ[ƒ‹
-	//		float factorPointLight = pow(saturate(-distancePointLight / pointLight.radius + 1.0f), pointLight.decay);
-	//		// SpotLight‚Ì“üŽËŒõ
-	//		float3 spotLightDirectionOnSurface = normalize(input.worldPosition - spotLight.pos);
-	//		// SpotLight‚Ö‚Ì‹——£
-	//		float distanceSpotLight = length(spotLight.pos - input.worldPosition);
-	//		// Žw”‚É‚æ‚éƒRƒ“ƒgƒ[ƒ‹
-	//		float factorSpotLight = pow(saturate(-distanceSpotLight / spotLight.distance + 1.0f), spotLight.decay);
-	//		// Camera•ûŒüŽZo
-	//		float3 toEye = normalize(worldPosition - input.worldPosition);
+			// PointLight‚Ì“üŽËŒõ
+			float3 pointLightDirection = normalize(input.worldPosition - pointLight.pos);
+			// PointLight‚Ö‚Ì‹——£
+			float distancePointLight = length(pointLight.pos - input.worldPosition);
+			// Žw”‚É‚æ‚éƒRƒ“ƒgƒ[ƒ‹
+			float factorPointLight = pow(saturate(-distancePointLight / pointLight.radius + 1.0f), pointLight.decay);
+			// SpotLight‚Ì“üŽËŒõ
+			float3 spotLightDirectionOnSurface = normalize(input.worldPosition - spotLight.pos);
+			// SpotLight‚Ö‚Ì‹——£
+			float distanceSpotLight = length(spotLight.pos - input.worldPosition);
+			// Žw”‚É‚æ‚éƒRƒ“ƒgƒ[ƒ‹
+			float factorSpotLight = pow(saturate(-distanceSpotLight / spotLight.distance + 1.0f), spotLight.decay);
+			// Camera•ûŒüŽZo
+			float3 toEye = normalize(worldPosition - input.worldPosition);
 
-	//		/*-------------------------------------------------------------------------------------------------*/
-	//		/// PointLight
+			/*-------------------------------------------------------------------------------------------------*/
+			/// PointLight
 
-	//		// “üŽËŒõ‚Ì”½ŽËƒxƒNƒgƒ‹‚ÌŒvŽZ
-	//		float3 reflectPointLight = reflect(pointLightDirection, normalize(input.normal));
-	//		float RdotEPointLight = dot(reflectPointLight, toEye);
-	//		float specularPowPointLight = pow(saturate(RdotEPointLight), shininess);
-	//		float NdotLPointLight = dot(normalize(input.normal), -pointLightDirection);
-	//		float cosPointLight = pow(NdotLPointLight * 0.5f + 0.5f, 2.0f);
-	//		// ŠgŽU”½ŽË
-	//		float3 diffusePointLight =
-	//		color.rgb * textureColor.rgb * pointLight.color.rgb * cosPointLight * pointLight.intensity * factorPointLight;
-	//		// ‹¾–Ê”½ŽË
-	//		float3 specularPointLight =
-	//		pointLight.color.rgb * pointLight.intensity * factorPointLight * specularPowPointLight * specularColor;
+			// “üŽËŒõ‚Ì”½ŽËƒxƒNƒgƒ‹‚ÌŒvŽZ
+			float3 reflectPointLight = reflect(pointLightDirection, normalize(input.normal));
+			float RdotEPointLight = dot(reflectPointLight, toEye);
+			float specularPowPointLight = pow(saturate(RdotEPointLight), shininess);
+			float NdotLPointLight = dot(normalize(input.normal), -pointLightDirection);
+			float cosPointLight = pow(NdotLPointLight * 0.5f + 0.5f, 2.0f);
+			// ŠgŽU”½ŽË
+			float3 diffusePointLight =
+			color.rgb * textureColor.rgb * pointLight.color.rgb * cosPointLight * pointLight.intensity * factorPointLight;
+			// ‹¾–Ê”½ŽË
+			float3 specularPointLight =
+			pointLight.color.rgb * pointLight.intensity * factorPointLight * specularPowPointLight * specularColor;
 			
-	//		/*-------------------------------------------------------------------------------------------------*/
-	//		/// SpotLight
+			/*-------------------------------------------------------------------------------------------------*/
+			/// SpotLight
 			
-	//		// “üŽËŒõ‚Ì”½ŽËƒxƒNƒgƒ‹‚ÌŒvŽZ
-	//		float3 reflectSpotLight = reflect(spotLightDirectionOnSurface, normalize(input.normal));
-	//		float RdotESpotLight = dot(reflectSpotLight, toEye);
-	//		float specularPowSpotLight = pow(saturate(RdotESpotLight), shininess);
-	//		float NdotLSpotLight = dot(normalize(input.normal), -spotLightDirectionOnSurface);
-	//		float cosSpotLight = pow(NdotLSpotLight * 0.5f + 0.5f, 2.0f);
-	//		float cosAngle = dot(spotLightDirectionOnSurface, spotLight.direction);
-	//		float falloffFactor = saturate((cosAngle - spotLight.cosAngle) / (spotLight.cosFalloffStart - spotLight.cosAngle));
-	//		// ŠgŽU”½ŽË
-	//		float3 diffuseSpotLight =
-	//		color.rgb * textureColor.rgb * spotLight.color.rgb * cosSpotLight * spotLight.intensity * factorSpotLight * falloffFactor;
-	//		// ‹¾–Ê”½ŽË
-	//		float3 specularSpotLight =
-	//		spotLight.color.rgb * spotLight.intensity * factorSpotLight * falloffFactor * specularPowSpotLight * specularColor;
+			// “üŽËŒõ‚Ì”½ŽËƒxƒNƒgƒ‹‚ÌŒvŽZ
+			float3 reflectSpotLight = reflect(spotLightDirectionOnSurface, normalize(input.normal));
+			float RdotESpotLight = dot(reflectSpotLight, toEye);
+			float specularPowSpotLight = pow(saturate(RdotESpotLight), shininess);
+			float NdotLSpotLight = dot(normalize(input.normal), -spotLightDirectionOnSurface);
+			float cosSpotLight = pow(NdotLSpotLight * 0.5f + 0.5f, 2.0f);
+			float cosAngle = dot(spotLightDirectionOnSurface, spotLight.direction);
+			float falloffFactor = saturate((cosAngle - spotLight.cosAngle) / (spotLight.cosFalloffStart - spotLight.cosAngle));
+			// ŠgŽU”½ŽË
+			float3 diffuseSpotLight =
+			color.rgb * textureColor.rgb * spotLight.color.rgb * cosSpotLight * spotLight.intensity * factorSpotLight * falloffFactor;
+			// ‹¾–Ê”½ŽË
+			float3 specularSpotLight =
+			spotLight.color.rgb * spotLight.intensity * factorSpotLight * falloffFactor * specularPowSpotLight * specularColor;
 
-	//		/*-------------------------------------------------------------------------------------------------*/
+			/*-------------------------------------------------------------------------------------------------*/
 
-	//		// ŠgŽU”½ŽË + ‹¾–Ê”½ŽË PointLight + SpotLight
-	//		output.color.rgb += diffusePointLight + specularPointLight + diffuseSpotLight + specularSpotLight;
-	//	}
+			// ŠgŽU”½ŽË + ‹¾–Ê”½ŽË PointLight + SpotLight
+			output.color.rgb += diffusePointLight + specularPointLight + diffuseSpotLight + specularSpotLight;
+		}
 		
-	//	if (enableBlinnPhongReflection == 1) {
+		if (enableBlinnPhongReflection == 1) {
 
-	//		// PointLight‚Ì“üŽËŒõ
-	//		float3 pointLightDirection = normalize(input.worldPosition - pointLight.pos);
-	//		// PointLight‚Ö‚Ì‹——£
-	//		float distancePointLight = length(pointLight.pos - input.worldPosition);
-	//		// Žw”‚É‚æ‚éƒRƒ“ƒgƒ[ƒ‹
-	//		float factorPointLight = pow(saturate(-distancePointLight / pointLight.radius + 1.0f), pointLight.decay);
-	//		// SpotLight‚Ì“üŽËŒõ
-	//		float3 spotLightDirectionOnSurface = normalize(input.worldPosition - spotLight.pos);
-	//		// SpotLight‚Ö‚Ì‹——£
-	//		float distanceSpotLight = length(spotLight.pos - input.worldPosition);
-	//		// Žw”‚É‚æ‚éƒRƒ“ƒgƒ[ƒ‹
-	//		float factorSpotLight = pow(saturate(-distanceSpotLight / spotLight.distance + 1.0f), spotLight.decay);
-	//		// Camera•ûŒüŽZo
-	//		float3 toEye = normalize(worldPosition - input.worldPosition);
+			// PointLight‚Ì“üŽËŒõ
+			float3 pointLightDirection = normalize(input.worldPosition - pointLight.pos);
+			// PointLight‚Ö‚Ì‹——£
+			float distancePointLight = length(pointLight.pos - input.worldPosition);
+			// Žw”‚É‚æ‚éƒRƒ“ƒgƒ[ƒ‹
+			float factorPointLight = pow(saturate(-distancePointLight / pointLight.radius + 1.0f), pointLight.decay);
+			// SpotLight‚Ì“üŽËŒõ
+			float3 spotLightDirectionOnSurface = normalize(input.worldPosition - spotLight.pos);
+			// SpotLight‚Ö‚Ì‹——£
+			float distanceSpotLight = length(spotLight.pos - input.worldPosition);
+			// Žw”‚É‚æ‚éƒRƒ“ƒgƒ[ƒ‹
+			float factorSpotLight = pow(saturate(-distanceSpotLight / spotLight.distance + 1.0f), spotLight.decay);
+			// Camera•ûŒüŽZo
+			float3 toEye = normalize(worldPosition - input.worldPosition);
 
-	//		/*-------------------------------------------------------------------------------------------------*/
-	//		/// PointLight
+			/*-------------------------------------------------------------------------------------------------*/
+			/// PointLight
 
-	//		float3 halfVectorPointLight = normalize(-pointLightDirection + toEye);
-	//		float NDotHPointLight = dot(normalize(input.normal), halfVectorPointLight);
-	//		float specularPowPointLight = pow(saturate(NDotHPointLight), shininess);
+			float3 halfVectorPointLight = normalize(-pointLightDirection + toEye);
+			float NDotHPointLight = dot(normalize(input.normal), halfVectorPointLight);
+			float specularPowPointLight = pow(saturate(NDotHPointLight), shininess);
 
-	//		float NdotLPointLight = dot(normalize(input.normal), -pointLightDirection);
-	//		float cosPointLight = pow(NdotLPointLight * 0.5f + 0.5f, 2.0f);
+			float NdotLPointLight = dot(normalize(input.normal), -pointLightDirection);
+			float cosPointLight = pow(NdotLPointLight * 0.5f + 0.5f, 2.0f);
 
-	//		// ŠgŽU”½ŽË
-	//		float3 diffusePointLight =
-	//		color.rgb * textureColor.rgb * pointLight.color.rgb * cosPointLight * pointLight.intensity * factorPointLight;
+			// ŠgŽU”½ŽË
+			float3 diffusePointLight =
+			color.rgb * textureColor.rgb * pointLight.color.rgb * cosPointLight * pointLight.intensity * factorPointLight;
 
-	//		// ‹¾–Ê”½ŽË
-	//		float3 specularPointLight =
-	//		pointLight.color.rgb * pointLight.intensity * factorPointLight * specularPowPointLight * specularColor;
+			// ‹¾–Ê”½ŽË
+			float3 specularPointLight =
+			pointLight.color.rgb * pointLight.intensity * factorPointLight * specularPowPointLight * specularColor;
 
-	//		/*-------------------------------------------------------------------------------------------------*/
-	//		/// SpotLight
+			/*-------------------------------------------------------------------------------------------------*/
+			/// SpotLight
 
-	//		float3 halfVectorSpotLight = normalize(-spotLightDirectionOnSurface + toEye);
-	//		float NDotHSpotLight = dot(normalize(input.normal), halfVectorSpotLight);
-	//		float specularPowSpotLight = pow(saturate(NDotHSpotLight), shininess);
+			float3 halfVectorSpotLight = normalize(-spotLightDirectionOnSurface + toEye);
+			float NDotHSpotLight = dot(normalize(input.normal), halfVectorSpotLight);
+			float specularPowSpotLight = pow(saturate(NDotHSpotLight), shininess);
 
-	//		float NdotLSpotLight = dot(normalize(input.normal), -spotLightDirectionOnSurface);
-	//		float cosSpotLight = pow(NdotLSpotLight * 0.5f + 0.5f, 2.0f);
+			float NdotLSpotLight = dot(normalize(input.normal), -spotLightDirectionOnSurface);
+			float cosSpotLight = pow(NdotLSpotLight * 0.5f + 0.5f, 2.0f);
 
-	//		float cosAngle = dot(spotLightDirectionOnSurface, spotLight.direction);
-	//		float falloffFactor = saturate((cosAngle - spotLight.cosAngle) / (spotLight.cosFalloffStart - spotLight.cosAngle));
+			float cosAngle = dot(spotLightDirectionOnSurface, spotLight.direction);
+			float falloffFactor = saturate((cosAngle - spotLight.cosAngle) / (spotLight.cosFalloffStart - spotLight.cosAngle));
 
-	//		// ŠgŽU”½ŽË
-	//		float3 diffuseSpotLight =
-	//		color.rgb * textureColor.rgb * spotLight.color.rgb * cosSpotLight * spotLight.intensity * falloffFactor * factorSpotLight;
+			// ŠgŽU”½ŽË
+			float3 diffuseSpotLight =
+			color.rgb * textureColor.rgb * spotLight.color.rgb * cosSpotLight * spotLight.intensity * falloffFactor * factorSpotLight;
 
-	//		// ‹¾–Ê”½ŽË
-	//		float3 specularSpotLight =
-	//		spotLight.color.rgb * spotLight.intensity * falloffFactor * factorSpotLight * specularPowSpotLight * specularColor;
+			// ‹¾–Ê”½ŽË
+			float3 specularSpotLight =
+			spotLight.color.rgb * spotLight.intensity * falloffFactor * factorSpotLight * specularPowSpotLight * specularColor;
 
-	//		/*-------------------------------------------------------------------------------------------------*/
+			/*-------------------------------------------------------------------------------------------------*/
 
-	//		// ŠgŽU”½ŽË + ‹¾–Ê”½ŽË LightDirecion + PointLight + SpotLight
-	//		output.color.rgb += diffusePointLight + specularPointLight + diffuseSpotLight + specularSpotLight;
-	//	}
-	//} else {
+			// ŠgŽU”½ŽË + ‹¾–Ê”½ŽË LightDirecion + PointLight + SpotLight
+			output.color.rgb += diffusePointLight + specularPointLight + diffuseSpotLight + specularSpotLight;
+		}
+	} else {
 
-	//	output.color.rgb = color.rgb * textureColor.rgb;
-	//}
+		output.color.rgb = color.rgb * textureColor.rgb;
+	}
 	
-	////========================================================================*/
-	////* emission *//
+	//========================================================================*/
+	//* emission *//
 	
-	//// ”­ŒõF
-	//float3 emission = emissionColor * emissiveIntensity;
-	//// Emission‚ð‰ÁŽZ
-	//output.color.rgb += emission * textureColor.rgb;
+	// ”­ŒõF
+	float3 emission = emissionColor * emissiveIntensity;
+	// Emission‚ð‰ÁŽZ
+	output.color.rgb += emission * textureColor.rgb;
 	
-	////========================================================================*/
-	////* shadow *//
+	//========================================================================*/
+	//* shadow *//
 	
-	//// wœŽZ‚Å³‹K‰»ƒXƒNƒŠ[ƒ“À•WŒn‚É•ÏŠ·‚·‚é
-	//float2 shadowMapUV = input.positionInLVP.xy / input.positionInLVP.w;
-	//shadowMapUV *= float2(0.5f, -0.5f);
-	//shadowMapUV += 0.5f;
+	// wœŽZ‚Å³‹K‰»ƒXƒNƒŠ[ƒ“À•WŒn‚É•ÏŠ·‚·‚é
+	float2 shadowMapUV = input.positionInLVP.xy / input.positionInLVP.w;
+	shadowMapUV *= float2(0.5f, -0.5f);
+	shadowMapUV += 0.5f;
 	
-	//// ƒ‰ƒCƒgƒrƒ…[ƒXƒNƒŠ[ƒ“‹óŠÔ‚Å‚ÌZ’l‚ðŒvŽZ‚·‚é
-	//float zInLVp = input.positionInLVP.z / input.positionInLVP.w;
+	// ƒ‰ƒCƒgƒrƒ…[ƒXƒNƒŠ[ƒ“‹óŠÔ‚Å‚ÌZ’l‚ðŒvŽZ‚·‚é
+	float zInLVp = input.positionInLVP.z / input.positionInLVP.w;
 	
-	//if (shadowMapUV.x > 0.0f && shadowMapUV.x < 1.0f &&
-	//	shadowMapUV.y > 0.0f && shadowMapUV.y < 1.0f) {
+	if (shadowMapUV.x > 0.0f && shadowMapUV.x < 1.0f &&
+		shadowMapUV.y > 0.0f && shadowMapUV.y < 1.0f) {
 		
-	//	// –@ü‚ÆŒõ‚Ì•ûŒü‚Ì“àÏ‚ðŒvŽZ
-	//	float NdotL = max(0.0f, dot(input.normal, directionalLight.direction));
+		// –@ü‚ÆŒõ‚Ì•ûŒü‚Ì“àÏ‚ðŒvŽZ
+		float NdotL = max(0.0f, dot(input.normal, directionalLight.direction));
 
-	//	float slopeScale = 1.0f;
-	//	float constantBias = 0.001f;
-	//	float bias = NdotL + constantBias;
+		float slopeScale = 1.0f;
+		float constantBias = 0.001f;
+		float bias = NdotL + constantBias;
 		
-	//	// –@ü‚ª‰¡Œü‚«‚È‚ç‰e‚ð–³Œø‰»
-	//	if (abs(input.normal.y) < 0.1f) {
-	//		bias = 1.0f;
-	//	}
+		// –@ü‚ª‰¡Œü‚«‚È‚ç‰e‚ð–³Œø‰»
+		if (abs(input.normal.y) < 0.1f) {
+			bias = 1.0f;
+		}
 
-	//	// ƒVƒƒƒhƒEƒ}ƒbƒv‚Ì[“x’l‚ðŽæ“¾
-	//	float zInShadowMap = gShadowTexture.Sample(gSampler, shadowMapUV).r;
+		// ƒVƒƒƒhƒEƒ}ƒbƒv‚Ì[“x’l‚ðŽæ“¾
+		float zInShadowMap = gShadowTexture.Sample(gSampler, shadowMapUV).r;
 
-	//	if (zInLVp - bias > zInShadowMap) {
+		if (zInLVp - bias > zInShadowMap) {
 			
-	//		// ŽÕ•Á—¦‚ÌŽæ“¾
-	//		float shadow = gShadowTexture.SampleCmpLevelZero(
-	//		gShadowSampler, shadowMapUV, zInLVp);
+			// ŽÕ•Á—¦‚ÌŽæ“¾
+			float shadow = gShadowTexture.SampleCmpLevelZero(
+			gShadowSampler, shadowMapUV, zInLVp);
 
-	//		float3 shadowColor = output.color.rgb * shadowRate;
-	//		output.color.rgb = lerp(output.color.rgb, shadowColor, shadow);
-	//	}
-	//}
+			float3 shadowColor = output.color.rgb * shadowRate;
+			output.color.rgb = lerp(output.color.rgb, shadowColor, shadow);
+		}
+	}
 
-	//// ƒ¿’l
-	//output.color.a = color.a * textureColor.a;
+	// ƒ¿’l
+	output.color.a = color.a * textureColor.a;
 	
 	return output;
 }
--- Project/Log/log.md
+++ Project/Log/log.md
@@ -3,6 +3,5 @@ EngineInformation:     Use Adapter:NVIDIA GeForce RTX 3060 Laptop GPU
 
 EngineInformation:     12.2
 EngineInformation:     load texture: uvChecker
-EngineInformation:     load texture: white
 EngineInformation:     load texture: checkerBoard
 EngineInformation:     load model: teapot
