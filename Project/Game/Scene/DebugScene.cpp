874ae2b79953f507d04f4c720b956ef96be7f72d
MurataSakuya <144077681+Muratasakuya@users.noreply.github.com>
2025-04-07 00:36:38 +0900

    debug用のsceneをさらにしました

--- Project/Game/Scene/DebugScene.cpp
+++ Project/Game/Scene/DebugScene.cpp
@@ -26,25 +26,11 @@ void DebugScene::Init(
 	//	load
 	//========================================================================
 
-	asset->LoadTexture("uvChecker");
-
-	asset->LoadModel("teapot");
-	asset->LoadModel("suzanne");
-	asset->LoadModel("multiMaterial");
-
 	//========================================================================
 	//	postProcess
 	//========================================================================
 
 	postProcessManager_ = postProcessManager;
-
-	const std::vector<PostProcess> postProcesses = {
-
-		PostProcess::Bloom,
-	};
-
-	postProcessManager_->Create(postProcesses);
-	postProcessManager_->AddProcess(PostProcess::Bloom);
 
 	//========================================================================
 	//	camera
@@ -60,47 +46,6 @@ void DebugScene::Init(
 	//	initObject
 	//========================================================================
 
-	const uint32_t kNumObject = 320;
-	const float offset = 6.0f;
-	const float offsetY = 4.0f;
-	const uint32_t gridSize = static_cast<uint32_t>(std::sqrt(kNumObject));
-	for (uint32_t index = 0; index < kNumObject; ++index) {
-
-		EntityID id = GameObjectHelper::CreateObject3D("multiMaterial", "multiMaterial", "MultiMaterial");
-		auto transform = Component::GetComponent<Transform3DComponent>(id);
-
-		uint32_t x = index % gridSize;
-		uint32_t z = index / gridSize;
-
-		transform->translation.x = x * offset;
-		transform->translation.z = z * offset;
-	}
-
-	for (uint32_t index = 0; index < kNumObject; ++index) {
-
-		EntityID id = GameObjectHelper::CreateObject3D("teapot", "teapot", "Teapot");
-		auto transform = Component::GetComponent<Transform3DComponent>(id);
-
-		uint32_t x = index % gridSize;
-		uint32_t z = index / gridSize;
-
-		transform->translation.x = x * offset;
-		transform->translation.y = offsetY;
-		transform->translation.z = z * offset;
-	}
-
-	for (uint32_t index = 0; index < kNumObject; ++index) {
-
-		EntityID id = GameObjectHelper::CreateObject3D("suzanne", "suzanne", "Suzanne");
-		auto transform = Component::GetComponent<Transform3DComponent>(id);
-
-		uint32_t x = index % gridSize;
-		uint32_t z = index / gridSize;
-
-		transform->translation.x = x * offset;
-		transform->translation.y = offsetY * 2.0f;
-		transform->translation.z = z * offset;
-	}
 }
 
 void DebugScene::Update([[maybe_unused]] SceneManager* sceneManager) {
