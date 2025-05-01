619fde2e9a28ca7bc610879f17e924ebc2d677bb
MurataSakuya <144077681+Muratasakuya@users.noreply.github.com>
2025-05-01 14:20:54 +0900

    体の向きが戻すように修正

--- Project/Game/Object3D/Player/Behavior/Parts/Body/BodyWaitBehavior.cpp
+++ Project/Game/Object3D/Player/Behavior/Parts/Body/BodyWaitBehavior.cpp
@@ -12,10 +12,14 @@
 
 BodyWaitBehavior::BodyWaitBehavior(const Json& data) {
 
+	undoPosY_ = std::make_unique<SimpleAnimation<float>>();
 	if (data.contains(waitBehaviorJsonKey_)) {
 
 		speed_ = JsonAdapter::GetValue<float>(data[waitBehaviorJsonKey_], "speed_");
 		amplitude_ = JsonAdapter::GetValue<float>(data[waitBehaviorJsonKey_], "amplitude_");
+		basePosY_ = JsonAdapter::GetValue<float>(data[waitBehaviorJsonKey_], "basePosY_");
+		rotationLerpRate_ = JsonAdapter::GetValue<float>(data[waitBehaviorJsonKey_], "rotationLerpRate_");
+		undoPosY_->FromJson(data[waitBehaviorJsonKey_]["UndoPosY"]);
 	}
 }
 
@@ -23,8 +27,36 @@ void BodyWaitBehavior::Execute(BasePlayerParts* parts) {
 
 	if (!baseTranslate_.has_value()) {
 
+		// animation開始
+		undoPosY_->Start();
 		// 初期座標を取得する
+		undoPosY_->move_.start = parts->GetTransform().translation.y;
 		baseTranslate_ = parts->GetTransform().translation;
+	}
+
+	// 現在の回転を取得
+	Quaternion rotation = parts->GetTransform().rotation;
+	// 正面向きの回転、Y軸は0.0f
+	Vector3 direction = parts->GetTransform().GetForward();
+	direction.y = 0.0f;
+	direction = Vector3::Normalize(direction);
+	Quaternion targetRotation = Quaternion::LookRotation(direction, Vector3(0.0f, 1.0f, 0.0f));
+
+	// 現在回転が初期化値でなければ補間する
+	if (rotation != targetRotation) {
+
+		// 初期化値に向かって補間
+		rotation = Quaternion::Slerp(rotation, targetRotation, rotationLerpRate_);
+		Quaternion worldRotation = Quaternion::Normalize(rotation);
+		parts->SetRotate(worldRotation);
+	}
+
+	// Y座標をまずbasePosYまで持って行ってからふわふわさせる
+	if (baseTranslate_.value().y != basePosY_) {
+
+		undoPosY_->LerpValue(baseTranslate_.value().y);
+		parts->SetTranslate(baseTranslate_.value());
+		return;
 	}
 
 	// 時間を加算
@@ -40,6 +72,7 @@ void BodyWaitBehavior::Execute(BasePlayerParts* parts) {
 void BodyWaitBehavior::Reset() {
 
 	// 初期化する
+	undoPosY_->Reset();
 	baseTranslate_ = std::nullopt;
 	elapsed_ = 0.0f;
 }
@@ -48,8 +81,12 @@ void BodyWaitBehavior::ImGui() {
 
 	ImGui::PushItemWidth(itemWidth_);
 
+	ImGui::DragFloat("basePosY", &basePosY_, 0.01f);
+	ImGui::DragFloat("rotationLerpRate", &rotationLerpRate_, 0.01f);
 	ImGui::DragFloat("speed", &speed_, 0.01f);
 	ImGui::DragFloat("amplitude", &amplitude_, 0.01f);
+
+	undoPosY_->ImGui("BodyWaitBehavior_undoPosY_");
 
 	ImGui::PopItemWidth();
 }
@@ -57,6 +94,9 @@ void BodyWaitBehavior::ImGui() {
 void BodyWaitBehavior::SaveJson(Json& data) {
 
 	// 値の保存
+	data[waitBehaviorJsonKey_]["basePosY_"] = basePosY_;
+	data[waitBehaviorJsonKey_]["rotationLerpRate_"] = rotationLerpRate_;
 	data[waitBehaviorJsonKey_]["speed_"] = speed_;
 	data[waitBehaviorJsonKey_]["amplitude_"] = amplitude_;
+	undoPosY_->ToJson(data[waitBehaviorJsonKey_]["UndoPosY"]);
 }
--- Project/Game/Object3D/Player/Behavior/Parts/Body/BodyWaitBehavior.h
+++ Project/Game/Object3D/Player/Behavior/Parts/Body/BodyWaitBehavior.h
@@ -37,9 +37,15 @@ private:
 
 	std::optional<Vector3> baseTranslate_; // 初期座標
 
+	// Y座標を元に戻す
+	std::unique_ptr<SimpleAnimation<float>> undoPosY_;
+
 	// parameter
 	// ふわふわさせるための変数
+	float basePosY_;  // 規定のY座標
 	float elapsed_;   // 時間経過
 	float amplitude_; // 振幅
 	float speed_;     // 速さ
+	// 回転補間割合
+	float rotationLerpRate_;
 };
