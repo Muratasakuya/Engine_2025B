f52bff35ab7c633c481ce705002d33b5baf86dd0
MurataSakuya <144077681+Muratasakuya@users.noreply.github.com>
2025-05-10 03:01:59 +0900

    instancing処理用のCS、MSの書き換え、設計を立てた

--- Project/Engine/Core/Component/Store/AnimationStore.h
+++ Project/Engine/Core/Component/Store/AnimationStore.h
@@ -6,6 +6,13 @@
 #include <Engine/Core/Component/AnimationComponent.h>
 #include <Engine/Core/Component/Base/IComponent.h>
 
+// skinning設計
+// skeleton、skinClusterはcomponentごとに必要
+// animationDataは読み込んだ時点のデータ1つでいい
+// CreateObject3Dした時にskinClusterのbuffer(palette、influence)、IO頂点bufferを作成する(bufferSize * インスタンス数分)
+// 各componentにはskeleton、skinClusterを持たせて各々更新できるようにする
+// dispatch処理はInstancedMeshにやらせる(予定)
+
 //============================================================================
 //	AnimationStore class
 //============================================================================
@@ -25,8 +32,6 @@ public:
 	void Update() override;
 
 	//--------- accessor -----------------------------------------------------
-
-		//--------- accessor -----------------------------------------------------
 
 	AnimationComponent* GetComponent(uint32_t entityId) override;
 	std::vector<AnimationComponent*> GetComponentList([[maybe_unused]] uint32_t entityId) override { return { nullptr }; }
