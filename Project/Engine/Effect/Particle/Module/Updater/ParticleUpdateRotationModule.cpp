ad7a0307788480eb05460b186520111182bdafcf
MurataSakuya <144077681+Muratasakuya@users.noreply.github.com>
2025-07-29 12:16:22 +0900

    回転に軸固定を追加

--- Project/Engine/Effect/Particle/Module/Updater/ParticleUpdateRotationModule.cpp
+++ Project/Engine/Effect/Particle/Module/Updater/ParticleUpdateRotationModule.cpp
@@ -9,30 +9,71 @@
 //	ParticleUpdateRotationModule classMethods
 //============================================================================
 
-void ParticleUpdateRotationModule::Execute(
-	CPUParticle::ParticleData& particle, [[maybe_unused]] float deltaTime) {
+Vector3 ParticleUpdateRotationModule::UpdateRotation(CPUParticle::ParticleData& particle) const {
 
-	particle.transform.billboardMode = static_cast<uint32_t>(billboardType_);
 	Vector3 rotation{};
+
+	// 更新処理分岐
+	switch (updateType_) {
+	case ParticleUpdateRotationModule::UpdateType::Lerp:
+
+		// 回転補間
+		rotation = Vector3::Lerp(lerpRotation_.start,
+			lerpRotation_.target, EasedValue(easing_, particle.progress));
+		break;
+	case ParticleUpdateRotationModule::UpdateType::ConstantAdd:
+
+		// 回転加算
+		rotation = particle.transform.rotationMatrix.GetRotationValue();
+		rotation += addRotation_;
+		break;
+	case ParticleUpdateRotationModule::UpdateType::MoveToDirection:
+
+		// 進行方向に向かせる
+		// 進行方向を取得
+		Vector3 direction = particle.velocity.Normalize();
+		rotation.y = std::atan2(direction.x, direction.z);
+		// 横軸方向の長さを求める
+		float horizontalLength = std::sqrtf(direction.x * direction.x + direction.z * direction.z);
+		// X軸周りの角度(θx)
+		rotation.x = std::atan2(-direction.y, horizontalLength);
+		break;
+	}
+	return rotation;
+}
+
+Vector3 ParticleUpdateRotationModule::LockAxis(const Vector3& rotation) {
+
+	// そのまま返す
+	if (lockAxisType_ == LockAxisType::None) {
+		return rotation;
+	}
+
+	// 回転軸の固定
+	Vector3 lockedRotation = rotation;
+	switch (lockAxisType_) {
+	case ParticleUpdateRotationModule::LockAxisType::AxisX:
+
+		lockedRotation.x = lockAxis_.x;
+		break;
+	case ParticleUpdateRotationModule::LockAxisType::AxisY:
+
+		lockedRotation.y = lockAxis_.y;
+		break;
+	case ParticleUpdateRotationModule::LockAxisType::AxisZ:
+
+		lockedRotation.z = lockAxis_.z;
+		break;
+	}
+	return lockedRotation;
+}
+
+void ParticleUpdateRotationModule::UpdateMatrix(
+	CPUParticle::ParticleData& particle, const Vector3& rotation) {
+
 	switch (billboardType_) {
 	case ParticleBillboardType::None:
 	case ParticleBillboardType::YAxis:
-
-		// 更新処理分岐
-		switch (updateType_) {
-		case ParticleUpdateRotationModule::UpdateType::Lerp:
-
-			// 回転補間
-			rotation = Vector3::Lerp(lerpRotation_.start,
-				lerpRotation_.target, EasedValue(easing_, particle.progress));
-			break;
-		case ParticleUpdateRotationModule::UpdateType::ConstantAdd:
-
-			// 回転加算
-			rotation = particle.transform.rotationMatrix.GetRotationValue();
-			rotation += addRotation_;
-			break;
-		}
 
 		// 回転行列の更新
 		particle.transform.rotationMatrix = Matrix4x4::MakeRotateMatrix(rotation);
@@ -44,10 +85,26 @@ void ParticleUpdateRotationModule::Execute(
 	}
 }
 
+void ParticleUpdateRotationModule::Execute(
+	CPUParticle::ParticleData& particle, [[maybe_unused]] float deltaTime) {
+
+	particle.transform.billboardMode = static_cast<uint32_t>(billboardType_);
+
+	// 回転の更新
+	Vector3 rotation = UpdateRotation(particle);
+
+	// 回転軸の固定
+	rotation = LockAxis(rotation);
+
+	// 行列の更新
+	UpdateMatrix(particle, rotation);
+}
+
 void ParticleUpdateRotationModule::ImGui() {
 
 	EnumAdapter<ParticleBillboardType>::Combo("billboardType", &billboardType_);
 	EnumAdapter<UpdateType>::Combo("updateType", &updateType_);
+	EnumAdapter<LockAxisType>::Combo("lockAxisType", &lockAxisType_);
 
 	switch (updateType_) {
 	case ParticleUpdateRotationModule::UpdateType::Lerp:
@@ -60,6 +117,21 @@ void ParticleUpdateRotationModule::ImGui() {
 	case ParticleUpdateRotationModule::UpdateType::ConstantAdd:
 
 		ImGui::DragFloat3("addRotation", &addRotation_.x, 0.01f);
+		break;
+	}
+
+	switch (lockAxisType_) {
+	case ParticleUpdateRotationModule::LockAxisType::AxisX:
+
+		ImGui::DragFloat("lockAxisX", &lockAxis_.x, 0.01f);
+		break;
+	case ParticleUpdateRotationModule::LockAxisType::AxisY:
+
+		ImGui::DragFloat("lockAxisY", &lockAxis_.y, 0.01f);
+		break;
+	case ParticleUpdateRotationModule::LockAxisType::AxisZ:
+
+		ImGui::DragFloat("lockAxisZ", &lockAxis_.z, 0.01f);
 		break;
 	}
 }
--- Project/Engine/Effect/Particle/Module/Updater/ParticleUpdateRotationModule.h
+++ Project/Engine/Effect/Particle/Module/Updater/ParticleUpdateRotationModule.h
@@ -40,20 +40,40 @@ private:
 	enum class UpdateType {
 
 		Lerp,
-		ConstantAdd
+		ConstantAdd,
+		MoveToDirection
+	};
+
+	// 回転軸の固定
+	enum class LockAxisType {
+
+		None,
+		AxisX,
+		AxisY,
+		AxisZ,
 	};
 
 	//--------- variables ----------------------------------------------------
-	
+
 	// 回転補間
 	ParticleCommon::LerpValue<Vector3> lerpRotation_;
 
 	// 回転加算
 	Vector3 addRotation_;
 
+	// 回転固定
+	Vector3 lockAxis_;
+
 	// ビルボードの種類
 	ParticleBillboardType billboardType_;
 
 	EasingType easing_;
 	UpdateType updateType_;
+	LockAxisType lockAxisType_;
+
+	//--------- functions ----------------------------------------------------
+
+	Vector3 UpdateRotation(CPUParticle::ParticleData& particle) const;
+	Vector3 LockAxis(const Vector3& rotation);
+	void UpdateMatrix(CPUParticle::ParticleData& particle, const Vector3& rotation);
 };
