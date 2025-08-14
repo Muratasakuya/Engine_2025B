e515efba36af9f0c760aa27131ca145d26dee44a
MurataSakuya <144077681+Muratasakuya@users.noreply.github.com>
2025-08-14 22:45:28 +0900

    シーン遷移処理のパラメータを変更

--- Project/Game/Objects/SceneTransition/FadeTransition.cpp
+++ Project/Game/Objects/SceneTransition/FadeTransition.cpp
@@ -20,7 +20,7 @@ void FadeTransition::Init() {
 	// fade初期設定
 	fadeSprite_->SetCenterTranslation();
 	fadeSprite_->SetSize(Vector2(Config::kWindowWidthf, Config::kWindowHeightf));
-	fadeSprite_->SetColor(Color::Convert(0x1F1F1FFF));
+	fadeSprite_->SetColor(Color::Convert(0x040404FF));
 	fadeSprite_->SetAlpha(0.0f);
 
 	loadSprite_ = std::make_unique<GameObject2D>();
