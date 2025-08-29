ba1aa827aa06a90eebf7e277a62d6891663f0d6b
MurataSakuya <144077681+Muratasakuya@users.noreply.github.com>
2025-08-29 15:17:29 +0900

    スキル攻撃で敵を突き抜けるようにした

--- Project/Game/Objects/GameScene/Player/State/States/PlayerSkilAttackState.cpp
+++ Project/Game/Objects/GameScene/Player/State/States/PlayerSkilAttackState.cpp
@@ -14,138 +14,115 @@
 
 void PlayerSkilAttackState::Enter(Player& player) {
 
-	player.SetNextAnimation("player_skilAttack", false, nextAnimDuration_);
-
+	// 敵が攻撃可能範囲にいるかチェック
 	const Vector3 playerPos = player.GetTranslation();
-	const Vector3 enemyPos = bossEnemy_->GetTranslation();
-	// 向き
-	Vector3 direction = (playerPos - enemyPos).Normalize();
+	const Vector3 bossEnemyPos = bossEnemy_->GetTranslation();
+	Vector3 direction = bossEnemyPos - playerPos;
+	assisted_ = CheckInRange(attackPosLerpCircleRange_,
+		Vector3(direction).Length());
 
-	// 補間座標を設定する
-	startPos_ = playerPos;
-	targetPos_ = enemyPos + direction * moveDistance_;
+	// 範囲内にいない場合はスキル攻撃を出来ない
+	if (assisted_) {
 
-	// 敵の方向を向かせる
-	player.SetRotation(Quaternion::LookRotation((enemyPos - playerPos).Normalize(), Vector3(0.0f, 1.0f, 0.0f)));
+		// 補完座標を設定
+		startPos_ = playerPos;
+		direction = direction.Normalize();
+		targetPos_ = bossEnemyPos + direction * moveValue_;
+		// アニメーションを設定
+		player.SetNextAnimation("player_skilAttack", false, nextAnimDuration_);
+		// 敵の方を向ける
+		player.SetRotation(Quaternion::LookRotation(direction, Vector3(0.0f, 1.0f, 0.0f)));
+	} else {
 
-	canExit_ = false;
-	currentState_ = State::Approach;
+		canExit_ = true;
+		// 確実に終了するようにする
+		exitTimer_ = exitTime_ * 2.0f;
+	}
 }
 
 void PlayerSkilAttackState::Update(Player& player) {
 
-	// 状態の更新
-	UpdateState(player);
-}
-
-void PlayerSkilAttackState::UpdateState(Player& player) {
-
-	switch (currentState_) {
-	case PlayerSkilAttackState::State::Approach: {
-
-		// 座標を補間
-		Vector3 translation = approach_.Update(startPos_, targetPos_);
-		player.SetTranslation(translation);
-
-		// 時間が経過しきったら次の状態に遷移させる
-		if (approach_.time < approach_.timer) {
-
-			currentState_ = State::Leave;
-			player.SetTranslation(targetPos_);
-		}
-		break;
+	if (!assisted_) {
+		return;
 	}
-	case PlayerSkilAttackState::State::Leave: {
 
-		// 座標を補間
-		Vector3 translation = leave_.Update(targetPos_, startPos_);
-		player.SetTranslation(translation);
+	// 前に前進させる
+	moveTimer_.Update();
+	Vector3 pos = Vector3::Lerp(startPos_, targetPos_, moveTimer_.easedT_);
+	player.SetTranslation(pos);
 
-		// 時間が経過しきったら状態を終了させる
-		if (leave_.time < leave_.timer) {
+	// animationが終わったかチェック
+	canExit_ = player.IsAnimationFinished();
+	// animationが終わったら時間経過を進める
+	if (canExit_) {
 
-			canExit_ = true;
-			player.SetTranslation(startPos_);
-		}
-		break;
+		exitTimer_ += GameTimer::GetScaledDeltaTime();
 	}
-	}
-}
-
-Vector3 PlayerSkilAttackState::LerpParameter::Update(
-	const Vector3& start, const Vector3& target) {
-
-	// 時間を進める
-	timer += GameTimer::GetScaledDeltaTime();
-	float lerpT = timer / time;
-	lerpT = EasedValue(easingType, lerpT);
-
-	return Vector3::Lerp(start, target, lerpT);
 }
 
 void PlayerSkilAttackState::Exit(Player& player) {
 
 	// リセット
 	canExit_ = false;
-	approach_.timer = 0.0f;
-	leave_.timer = 0.0f;
-	player.ResetAnimation();
-}
+	exitTimer_ = 0.0f;
+	moveTimer_.Reset();
 
-void PlayerSkilAttackState::LerpParameter::ImGui(const std::string& label) {
+	if (assisted_) {
 
-	ImGui::SeparatorText(label.c_str());
-
-	ImGui::DragFloat(("time##" + label).c_str(), &time, 0.01f);
-	Easing::SelectEasingType(easingType, label);
+		player.ResetAnimation();
+	}
 }
 
 void PlayerSkilAttackState::ImGui(const Player& player) {
 
 	ImGui::DragFloat("nextAnimDuration", &nextAnimDuration_, 0.001f);
 	ImGui::DragFloat("rotationLerpRate", &rotationLerpRate_, 0.001f);
-	ImGui::DragFloat("moveDistance", &moveDistance_, 0.1f);
+	ImGui::DragFloat("exitTime", &exitTime_, 0.01f);
 
-	approach_.ImGui("approach");
-	leave_.ImGui("leave");
+	PlayerBaseAttackState::ImGui(player);
 
-	const Vector3 playerPos = player.GetTranslation();
-	const Vector3 enemyPos = bossEnemy_->GetTranslation();
-	// 向き
-	Vector3 direction = (playerPos - enemyPos).Normalize();
+	moveTimer_.ImGui("MoveTimer");
+	ImGui::DragFloat("moveValue", &moveValue_, 0.1f);
 
-	// 補間座標を設定する
-	Vector3 startPos = playerPos;
-	Vector3 targetPos = enemyPos + direction * moveDistance_;
-	startPos.y = 4.0f;
-	targetPos.y = 4.0f;
+	LineRenderer* renderer = LineRenderer::GetInstance();
 
-	LineRenderer::GetInstance()->DrawLine3D(
-		startPos, targetPos, Color::Red());
+	Vector3 start = player.GetTranslation();
+	const Vector3 bossEnemyPos = bossEnemy_->GetTranslation();
+	Vector3 direction = Vector3(bossEnemyPos - start).Normalize();
+	Vector3 target = bossEnemyPos + direction * moveValue_;
+	start.y = 2.0f;
+	target.y = 2.0f;
+	renderer->DrawLine3D(start, target, Color::Cyan());
+	renderer->DrawSphere(8, 4.0f, target, Color::Cyan());
 }
 
 void PlayerSkilAttackState::ApplyJson(const Json& data) {
 
 	nextAnimDuration_ = JsonAdapter::GetValue<float>(data, "nextAnimDuration_");
 	rotationLerpRate_ = JsonAdapter::GetValue<float>(data, "rotationLerpRate_");
-	moveDistance_ = JsonAdapter::GetValue<float>(data, "moveDistance_");
+	exitTime_ = JsonAdapter::GetValue<float>(data, "exitTime_");
 
-	approach_.time = JsonAdapter::GetValue<float>(data, "approach_.time");
-	approach_.easingType = static_cast<EasingType>(
-		JsonAdapter::GetValue<int>(data, "approach_.easingType"));
-	leave_.time = JsonAdapter::GetValue<float>(data, "leave_.time");
-	leave_.easingType = static_cast<EasingType>(
-		JsonAdapter::GetValue<int>(data, "leave_.easingType"));
+	PlayerBaseAttackState::ApplyJson(data);
+
+	moveTimer_.FromJson(data.value("MoveTimer", Json()));
+	moveValue_ = data.value("moveValue_", 1.0f);
 }
 
 void PlayerSkilAttackState::SaveJson(Json& data) {
 
 	data["nextAnimDuration_"] = nextAnimDuration_;
 	data["rotationLerpRate_"] = rotationLerpRate_;
-	data["moveDistance_"] = moveDistance_;
+	data["exitTime_"] = exitTime_;
 
-	data["approach_.time"] = approach_.time;
-	data["approach_.easingType"] = static_cast<int>(approach_.easingType);
-	data["leave_.time"] = leave_.time;
-	data["leave_.easingType"] = static_cast<int>(leave_.easingType);
+	PlayerBaseAttackState::SaveJson(data);
+
+	moveTimer_.ToJson(data["MoveTimer"]);
+	data["moveValue_"] = moveValue_;
+}
+
+bool PlayerSkilAttackState::GetCanExit() const {
+
+	// 経過時間が過ぎたら
+	bool canExit = exitTimer_ > exitTime_;
+	return canExit;
 }
